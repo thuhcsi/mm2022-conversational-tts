0c41df32742aa297af3e3a499f608bcb2b5b59d5
Jingbei Li <i@jingbei.li>
2022-10-12 17:37:01 +0800

    updated ecc.py

--- data/ecc.py
+++ data/ecc.py
@@ -1,46 +1,42 @@
 import numpy as np
 import torch
 from pathlib import Path
+from torch.nn.utils.rnn import pad_sequence
 
-def process_bert(model, tokenizer, line):
-    inputs = tokenizer(line[1], return_tensors="pt")
-    outputs = model(**inputs)
-    np.save(line[2], outputs.last_hidden_state[0].detach().numpy())
-    np.save(line[3], outputs.pooler_output[0].detach().numpy())
+def sort_key(i):
+    return [int(k) for k in i.stem.split('.')[0].split('-')[:-1]]
+
+class Utterance:
+
+    def __init__(self, text):
+        with open(text) as f:
+            self.text = f.readline()
+        self.speaker = text.stem[-1]
+        self.wav = text.parent / f'{text.stem}.wav'
+        self.bert = text.parent / f'{text.stem}.bert.npy'
+        self.gst = text.parent / f'{text.stem}.gst.npy'
+        self.wst = text.parent / f'{text.stem}.bert.npy'
 
 class ECC(torch.utils.data.Dataset):
 
-    def __init__(self, path, segmented=None, chunk_size=5):
+    def __init__(self, segmented, chunk_size=6):
         super().__init__()
-        self.path = Path(path)
-        self.segmented = Path(segmented) if segmented else self.path / 'segmented'
+        self.path = Path(segmented)
         self.chunk_size = chunk_size
 
-        conversations = self.path / 'conversations'
+        texts = sorted([i for i in self.path.rglob('*.txt')], key=sort_key)
+
         self.conversations = []
-        for filename in conversations.rglob('*.txt'):
-            i = filename.name[21:23]
+        previous = None
+        for i in texts:
+            current = sort_key(i)[-1]
 
-            file = open(filename)
-            lines = file.readlines()
-            file.close()
+            if current - 1 != previous:
+                self.conversations.append([])
+            else:
+                self.conversations[-1].append(Utterance(i))
+            previous = current
 
-            breaks = [j for j, line in enumerate(lines) if line == '\n']
-            breaks.insert(0, -1)
-            if breaks[-1] != len(lines) - 1:
-                breaks.append(len(lines)-1)
-
-            lines = [i.strip('\r\n').split('\t')[::3] for i in lines]
-
-            self.conversations.append([lines[breaks[j]+1:breaks[j+1]] for j in range(len(breaks)-1)])
-            for j, conversation in enumerate(self.conversations[-1]):
-                for k, line in enumerate(conversation):
-                    self.conversations[-1][j][k].append(Path(self.segmented / f'{i}-{j}-{k}-{line[0]}.bert.npy'))
-                    self.conversations[-1][j][k].append(Path(self.segmented / f'{i}-{j}-{k}-{line[0]}.sbert.npy'))
-                    self.conversations[-1][j][k].append(Path(self.segmented / f'{i}-{j}-{k}-{line[0]}.gst.npy'))
-                    self.conversations[-1][j][k].append(Path(self.segmented / f'{i}-{j}-{k}-{line[0]}.wst.npy'))
-
-        self.conversations = [j for i in self.conversations for j in i]
         self.conversations = [i for i in self.conversations if len(i) >= chunk_size]
         self.chunks = [i[j:j+chunk_size] for i in self.conversations for j in range(len(i)-chunk_size)]
 
@@ -48,34 +44,51 @@ class ECC(torch.utils.data.Dataset):
         return len(self.chunks)
 
     def __getitem__(self, index):
-        result = []
+        speaker = []
+        length = []
+        bert = []
+        gst = []
+        wst = []
+        speaker_cache = ''
         for i in self.chunks[index]:
-            bert = np.load(i[2])
-            sbert = np.load(i[3])
-            #gst = np.load(self.chunks[index][4])
-            #wst = np.load(self.chunks[index][5])
-            result.append([bert, sbert])
+            if not i.speaker in speaker_cache:
+                speaker_cache += i.speaker
+            speaker.append(speaker_cache.find(i.speaker))
+            bert.append(torch.as_tensor(np.load(i.bert)))
+            length.append(bert[-1].shape[0])
+            gst.append(torch.as_tensor(np.load(i.gst)))
+            wst.append(torch.as_tensor(np.load(i.wst)))
+        speaker = np.array(speaker)
+        length = np.array(length)
+        bert = pad_sequence(bert, batch_first=True)
+        gst = torch.stack(gst)
+        wst = pad_sequence(wst)
+        return length, speaker, bert, gst, wst
 
-        return bert, sbert#, gst, wst
+def process_bert(model, tokenizer, utterance: Utterance):
+    inputs = tokenizer(utterance.text, return_tensors="pt")
+    outputs = model(**inputs)
+    np.save(utterance.bert, outputs.last_hidden_state[0].detach().numpy())
+    np.save(utterance.gst, outputs.pooler_output[0].detach().numpy())
 
 if __name__ == '__main__':
     import sys
     from functools import partial
     from tqdm.contrib.concurrent import process_map, thread_map
 
-    dataset = ECC(sys.argv[1], sys.argv[2])
+    dataset = ECC(sys.argv[1])
     print(len(dataset.conversations), len(dataset.chunks))
 
-    if not dataset.conversations[0][0][2].exists():
+    if not dataset.chunks[0][0].bert.exists():
         from transformers import AutoTokenizer, AutoModel
         model = AutoModel.from_pretrained("bert-base-uncased")
         tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-        thread_map(partial(process_bert, model, tokenizer), [line for conversation in dataset.conversations for line in conversation])
+        thread_map(partial(process_bert, model, tokenizer), [i for chunk in dataset.chunks for i in chunk])
 
-    #from .common import Collate
-    #data_loader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=Collate('cuda:0'), drop_last=True)
-    #for batch in data_loader:
-    #    for _list in batch:
-    #        print([i.shape for i in _list])
-    #        print([i.dtype for i in _list])
-    #    break
+    from .common import Collate
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=Collate('cuda:0'), drop_last=True)
+    for batch in data_loader:
+        for _list in batch:
+            print([i.shape for i in _list])
+            print([i.dtype for i in _list])
+        break
